d5b7e6548f36aed896b06ebc8e12eee5d96e60d7
Maxim <cala4yov.maks@gmail.com>
2022-03-03 15:31:00 +0300

    Это наш первый комит

---
+++ jsProjet/function.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var left = 1;
+let second = 2;
+const pi = 3.14;
+console.log(left);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[0]);
+
+//alert("Hello World!");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
